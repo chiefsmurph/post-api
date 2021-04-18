1de0d03f02b8277bdc6752de5d7a16635029986a
chiefsmurph <chiefsmurph@gmail.com>
2021-04-17 23:08:50 -0400

    oh yeah!

--- src/resources/post/post.controllers.js
+++ src/resources/post/post.controllers.js
@@ -1,4 +1,20 @@
 import { crudControllers } from '../../utils/crud'
 import { post } from './post.model'
+export default {
+  ...crudControllers(post),
+  getMany: async (req, res) => {
+    try {
+      const docs = await post
+        .find()
+        .sort({ _id: -1 })
+        .populate('createdBy', 'username')
+        .lean()
+        .exec()
 
-export default crudControllers(post)
+      res.status(200).json({ data: docs })
+    } catch (e) {
+      console.error(e)
+      res.status(400).end()
+    }
+  }
+}
